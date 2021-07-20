56bcd6dab0e768a587f0e61f290556fa4242210b
Alice Frosi <afrosi@redhat.com>
2021-07-20 15:50:47 +0200

    Fix deployment

--- hack/cluster-deploy.sh
+++ hack/cluster-deploy.sh
@@ -6,8 +6,9 @@ IMAGE_DRIVER="${IMAGE_DRIVER_NAME}:${TAG}"
 IMAGE_QSD="${IMAGE_QSD_NAME}:${TAG}"
 CLUSTER=k8s-qsd
 kubectl delete -f deployment/driver.yaml
-
+docker exec -ti k8s-qsd-control-plane crictl rmi ${IMAGE_DRIVER}
+docker exec -ti k8s-qsd-control-plane crictl rmi ${IMAGE_QSD}
 set -ex
 kind load docker-image --name ${CLUSTER}  ${IMAGE_DRIVER}
 kind load docker-image --name ${CLUSTER} ${IMAGE_QSD}
-kubectl apply -f deployment
+#kubectl apply -f deployment
