6837ccc04a4f2f3c13c2e11b3fd91396b9ca7114
Alice Frosi <afrosi@redhat.com>
2021-07-23 14:55:01 +0200

    Enable VolumeSnapshot CRDs

--- hack/cluster-create.sh
+++ hack/cluster-create.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 kind create cluster --name k8s-qsd --config hack/cluster/kind.yaml
+
+# Apply VolumeSnapshot CRDs
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
