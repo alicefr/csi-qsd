f8aeef532d81aff4fa936506f6dd1fd6a2832d2f
Alice Frosi <afrosi@redhat.com>
2021-09-01 09:04:32 +0200

    Add additional images

--- tests/create-block-graph.sh
+++ tests/create-block-graph.sh
@@ -13,3 +13,9 @@ bin/qsd-client  create --image test1 --from snap2
 bin/qsd-client  create --image test2 --from test0
 bin/qsd-client snapshot create --source test1 --name snap4
 bin/qsd-client snapshot create --source test1 --name snap5
+
+bin/qsd-client  create --image test3 --size 512000
+bin/qsd-client snapshot create --source test3 --name snap6
+
+bin/qsd-client  create --image test4 --size 512000
+bin/qsd-client snapshot create --source test4 --name snap7
