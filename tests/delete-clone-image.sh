f9667bac8962f484a718baaa8d16ae6c9a536adf
Alice Frosi <afrosi@redhat.com>
2021-09-20 10:49:07 +0200

    Update tests

--- tests/delete-clone-image.sh
+++ tests/delete-clone-image.sh
@@ -2,4 +2,5 @@
 
 bin/qsd-client  create --image test0 --size 512000
 bin/qsd-client  create --image test2 --from test0
+bin/qsd-client list --tree true
 bin/qsd-client delete image --image test2
--- tests/delete-images.sh
+++ tests/delete-images.sh
@@ -2,7 +2,8 @@
 
 set -ex
 tests/create-block-graph.sh
-
+bin/qsd-client list --tree true
 bin/qsd-client delete image --image test0
 bin/qsd-client delete image --image test1
 bin/qsd-client delete image --image test2
+bin/qsd-client list --tree true
